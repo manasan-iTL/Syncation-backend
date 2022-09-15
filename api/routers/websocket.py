c07a21a7b829babc6f7abff6169d75a06ab70805
manasan-iTL <daiyaofssh2020@gmail.com>
2022-09-15 12:45:13 +0900

    [add] websocket通信の復活

--- api/routers/websocket.py
+++ api/routers/websocket.py
@@ -17,13 +17,13 @@ async def join_room(sid, data):
     # ルームIDが存在するのかを判定する（DBにルームIDが存在しているか）
     print(data["roomId"])
     sio.enter_room(sid, data["roomId"])
-    await sio.emit("joined_room", {"id": sid, "username": data["username"]}, room=data["roomId"])
+    await sio.emit("joined_room", {"id": sid, "username": data["username"]}, room=data["roomId"], skip_sid=sid)
     return {"result": "Success"}
 
 @sio.event
 async def send_time(sid, data):
     print(data["type"] + "の時間：" + data["time"])
-    await sio.emit("receive_time", {"type": data["type"], "time": data["time"]}, room=data["roomId"])
+    await sio.emit("receive_time", {"type": data["type"], "time": data["time"]}, room=data["roomId"], skip_sid=sid)
 
 
 @sio.event
